2a8dfa80daaacda1986d78f0215cc1e48429dc6b
Emily Boegheim <20103559@tafe.wa.edu.au>
2025-05-30 18:49:27 +0800

    feat: call API and display results when form is submitted

--- assets/js/stars-and-planets.js
+++ assets/js/stars-and-planets.js
@@ -22,12 +22,105 @@ function checkOnlyOneBox(event) {
 }
 
 /**
+ * When the form Submit button is clicked, call the appropriate API and display
+ * the results.
+ * @param event The event triggered by clicking the Submit button.
+ */
+async function callApi(event) {
+  event.preventDefault();
+  const requestValues = getFormValues();
+  const requestUrl = buildApiUrl(requestValues);
+  await fetch(requestUrl, {
+    headers: {'X-Api-Key': apiKey}
+    })
+    .then(async function (response) {
+      if (!response.ok) {
+        // TODO: error handling
+      }
+      return await response.json();
+    })
+    .then(function (results) {
+      displayApiResults(results);
+    });
+}
+
+/**
+ * Get the values for the API request from the form and return them as an
+ * object.
+ * @returns {Object} with fields route and name, giving the API route to use and
+ *   the name parameter to pass to the API.
+ */
+function getFormValues() {
+  const selectedCheckbox = document.querySelector('#stars-or-planets input:checked');
+  if (!selectedCheckbox) {
+    // TODO: error handling
+    // stop here
+    return;
+  }
+
+  const apiRequestValues = new Object();
+  apiRequestValues.route = selectedCheckbox.dataset.api;
+  const optionsInputId = selectedCheckbox.dataset.options;
+  apiRequestValues.name = document.getElementById(optionsInputId).value;
+  return apiRequestValues;
+}
+
+/**
+ * Builds a URL for the API request.
+ * @param requestValues {Object} with fields route and name, giving the API
+ *   route to use and the name parameter to pass to the API.
+ * @returns {string} The API request URL as a string.
+ */
+function buildApiUrl(requestValues) {
+  const apiUrl = `https://api.api-ninjas.com/${requestValues.route}?` +
+    `name=${requestValues.name}`;
+  return apiUrl;
+}
+
+/**
+ * Build an HTML table from one star or planet object returned by the API.
+ * @param data {Object} An object containing the data of one star or planet.
+ * @returns {HTMLTableElement} A table element containing the API data.
+ */
+function buildResultsTable(data) {
+  const table = document.createElement('table');
+  let tableContents = `<thead><tr><th colspan="2" scope="col"><h3>${data.name}</h3></th></tr></thead>`;
+  tableContents += '<tbody>';
+  for (let property in data) {
+    if (property !== 'name') {
+      const propAsTitle = property.replaceAll('_', ' ');
+      tableContents += `<tr><th scope="row">${propAsTitle}</th><td>${data[property]}</td></tr>`
+    }
+  }
+  tableContents += '</tbody>';
+  table.innerHTML = tableContents;
+  return table;
+}
+
+/**
+ * Displays the results of the API request in the results section of the form.
+ * @param results {Object} The parsed results of the API request.
+ */
+function displayApiResults(results) {
+  const resultsBox = document.getElementById('form-results');
+  while (resultsBox.childElementCount > 1) {
+    resultsBox.lastElementChild.remove();
+  }
+  for (const index in results) {
+    const table = buildResultsTable(results[index]);
+    resultsBox.append(table);
+  }
+}
+
+/**
  * Initialise the Stars/Planets API request form.
  * @param event The window load event.
  */
 function initForm(event) {
   const starsOrPlanets = document.getElementById('stars-or-planets');
   starsOrPlanets.addEventListener('change', checkOnlyOneBox);
+  const apiRequestForm = document.getElementById('api-request-form');
+  apiRequestForm.addEventListener('submit', callApi);
 }
 
 window.addEventListener('load', initForm);
---
+++ phase-4/readme.md
@@ -0,0 +1,12 @@
+# Read Me
+
+## API Key
+
+This project uses two APIs from API Ninjas (https://api-ninjas.com). These require an API key for access.
+
+To run this project, add a file named "api-key.js" to the assets/js directory.
+
+The api-key.js file must contain the following line of code:
+```js
+const apiKey = 'your-api-ninjas-api-key-here';
+```
